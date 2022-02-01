9c0f4bb4ff9cb740f0cb236a9efac69fed8921cb
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2022-02-01 14:05:34 -0500

    removed extra comments

--- Main.java
+++ Main.java
@@ -1,14 +1,5 @@
 import casino.*;
-import java.util.Scanner;
 
-
-// I've finished the blackjack part, I think we could do like War (the card game), a slot machine, or craps (see below), roulette, uno?
-// https://en.wikipedia.org/wiki/Craps
-//I'll look up how to play craps after school since it seems a little compicated, but if you know all the rules and already know how to code that then we can
-//Else we could do war because it seems like a pretty easy one to do
-//roulette probably needs some sort of visual to it so idk about that
-//Do you know what all of the .class files do?
-// replit is weird and its how it complies java to run it
 class Main {
   public static void main(String[] args) {
      while (!Casino.isInDebt()) {
